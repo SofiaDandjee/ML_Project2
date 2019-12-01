85ed7c1382782a8f59695d8c5da58c437bfc5aee
Sofia Dandjee <sofia.dandjee@epfl.ch>
2019-12-01 11:38:33 +0100

    load data

---
+++ scripts/data_helpers.py
@@ -0,0 +1,42 @@
+import numpy as np
+import scipy.sparse as sp
+
+
+def read_txt(path):
+    """read text file from path."""
+    with open(path, "r") as f:
+        return f.read().splitlines()
+
+
+def load_data(path_dataset):
+    """Load data in text format, one rating per line, as in the kaggle competition."""
+    data = read_txt(path_dataset)[1:]
+    return preprocess_data(data)
+
+
+def preprocess_data(data):
+    """preprocessing the text data, conversion to numerical array format."""
+    def deal_line(line):
+        pos, rating = line.split(',')
+        row, col = pos.split("_")
+        row = row.replace("r", "")
+        col = col.replace("c", "")
+        return int(row), int(col), float(rating)
+
+    def statistics(data):
+        row = set([line[0] for line in data])
+        col = set([line[1] for line in data])
+        return min(row), max(row), min(col), max(col)
+
+    # parse each line
+    data = [deal_line(line) for line in data]
+
+    # do statistics on the dataset.
+    min_row, max_row, min_col, max_col = statistics(data)
+    print("number of items: {}, number of users: {}".format(max_row, max_col))
+
+    # build rating matrix.
+    ratings = sp.lil_matrix((max_row, max_col))
+    for row, col, rating in data:
+        ratings[row - 1, col - 1] = rating
+    return ratings
---
+++ scripts/proj2_helpers.py
@@ -0,0 +1,50 @@
+import csv
+import numpy as np
+
+def baseline_global_mean(train, test):
+    """baseline method: use the global mean."""
+    
+    ##Global mean 
+    global_mean = np.sum(train [np.nonzero(train)])/train.nnz
+    
+    to_predict = test[np.nonzero(test)].todense()
+    
+    mse = calculate_mse (to_predict, global_mean)
+    
+    return np.sqrt((mse / test.nnz))
+
+def baseline_user_mean(train, test):
+    """baseline method: use the user means as the prediction."""
+    mse = 0
+    num_items, num_users = train.shape
+    
+    user_mean = np.zeros(num_users)
+    
+    for i in range (num_users):
+        train_i = train[:,i]
+        mean = np.mean(train_i[train_i.nonzero()])
+        
+        test_i = test[:,i]
+        to_predict = test_i[test_i.nonzero()].todense()
+    
+        mse += calculate_mse (to_predict, mean)
+    
+    return np.sqrt(mse/test.nnz)
+
+def baseline_item_mean(train, test):
+    """baseline method: use item means as the prediction."""
+    mse = 0
+    num_items, num_users = train.shape
+
+    item_mean = np.zeros(num_items)
+    
+    for i in range (num_items):
+        train_i = train[i,:]
+        mean = np.mean(train_i[train_i.nonzero()])
+        
+        test_i = test[i,:]
+        to_predict = test_i[test_i.nonzero()].todense()
+    
+        mse += calculate_mse (to_predict, mean)
+    
+    return np.sqrt(mse/test.nnz)
